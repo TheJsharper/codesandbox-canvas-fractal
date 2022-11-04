53137c860cf01788de61a9cb9f5d89fe8fd5bbba
Pedro Salomon <pedro.salomon@gmail.com>
2022-11-04 09:13:25 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 600;
+canvas.height = 600;
+console.log(canvas);
+class Factal {}
+
+class Partickle {}
+class Rain {}
